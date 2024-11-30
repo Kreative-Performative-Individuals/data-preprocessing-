e871e1ac0d2996d8863f520f34e5195501c7a4f7
Aura <aura.natale@uc.cl>
2024-11-30 12:20:02 +0100

    Implementation of pipeline with API connections

--- information.py
+++ information.py
@@ -82,3 +82,25 @@ class infoManager:
     def update_model_forecast(self, x, model):
         self.info[x['name']][x['asset_id']][x['kpi']][x['operation']][3]=model
 
+
+def create_info_object():
+    # Dictionary used to mantain a local version of the original kpis in order to handle the specific batches useful for the imputation and the data drift analysis.
+    info = {}
+
+    for m in list(machine.keys()):
+        info_asset = {}  # Reset for each machine
+        for id in list(machine[m]):
+            info_kpi = {}  # Reset for each asset
+            for k in list(kpi.keys()):
+                info_op={}
+                for o in kpi[k][1]:
+                    info_op[o] = [[[], [], [], [], []], [0, 0, 0, 0, 0],[], []]  # [0]: batch ([0][0]: 'sum', [0][1]: 'avg', [0][2]: 'min', [0][3]: 'max', [0][4] 'var'
+                                                                            # [1]: counter for missing values (same subdivision of the batch)
+                                                                            # [2]: trained model for anomaly detection.
+                info_kpi[k] = info_op
+            info_asset[id] = info_kpi  # Associate KPIs with the asset ID
+        info[m] = info_asset  # Associate assets with the machine type
+
+    im=infoManager(info)  
+
+    return im
